4608ed951653466d2a9905662d60f60c888ec3c3
Евгений Жуков <zhukov1414@yandex.ru>
2022-12-26 20:18:37 +0300

    Edit flake8 new

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -6,4 +6,4 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('group/<slug:slug>/', views.group_list, name='group_list'),]
+    path('group/<slug:slug>/', views.group_list, name='group_list'), ]
